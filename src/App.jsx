e203d3570899f68e952eac24b3a8d9e62f7eff0e
Anant <anantpatel07012003@gmail.com>
2023-10-04 18:08:54 +0530

    navigation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Homepage from "./pages/Homepage";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/page/:pageNumber" element={<Homepage />} />
-        </Routes>
-      </Router>
+      <Homepage />
     </Provider>
   );
 }
